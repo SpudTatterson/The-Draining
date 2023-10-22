5b55eaacf70ec09fd78afa02c6afd2420d4c9d69
SpudTatterson <tchcjhui123@gmail.com>
2023-10-22 13:09:44 +0300

    Added Screwdriver Class

---
+++ Assets/Scripts/Interaction/Screwdriver.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screwdriver : Interactable
+{
+    
+    public override void Interact(Interactor interactor)
+    {
+
+    }
+}
