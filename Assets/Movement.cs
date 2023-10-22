28e2af03973c1ff32774ff6a83984ab29f5b6807
Sunset4242 <129441440+Sunset4242@users.noreply.github.com>
2023-10-22 15:12:05 +0100

    Bugfix2:)

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    public Vector3 direction;
     // Start is called before the first frame update
     private void FixedUpdate()
     {
