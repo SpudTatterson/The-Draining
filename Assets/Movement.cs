4f977b3973a798095f2566e2c5dd4ede34cbb948
Sunset4242 <129441440+Sunset4242@users.noreply.github.com>
2023-10-22 09:12:44 +0100

    Added movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (horizontal != 0 || vertical != 0)
+        {
+            lastHorizontal = horizontal;
+            lastVertical = vertical;
+        }
+        direction = new Vector3(horizontal, vertical, 0);
+        transform.position += direction * speed * Time.deltaTime;
+
+    }
+}
